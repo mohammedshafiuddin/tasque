3ad018056a3b3e417dc6a67177349f7852b3741a
mohammedshafiuddin <mohammedshafiuddin54@gmail.com>
2024-10-26 01:09:57 +0530

    added fs.readFIle for ssl reading

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path'
 import https from 'https';
+import fs from 'fs';
 
 
 const app = express();
@@ -9,10 +10,12 @@ app.use('/',(req,res) => {
     res.send('Hello World');
 })
 
+
 const server = https.createServer({
-    key: path.join(__dirname, 'assets','certs', 'private.key'),
-    cert: path.join(__dirname, 'assets','certs', 'certificate.cert')   
+    key: fs.readFileSync(path.join(__dirname,'../assets/certs/private.key')),
+    cert: fs.readFileSync(path.join(__dirname,'../assets/certs/certificate.crt'))   
 }, app);
+
 
 server.listen(3000, () => {
     console.log('Server is running on https://localhost:3000');
